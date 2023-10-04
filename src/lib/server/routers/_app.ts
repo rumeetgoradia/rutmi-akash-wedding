a84fa944aaad3885c3fcce1eabe5a6e8e2e971b8
Rumeet Goradia <rumeet.goradia@gmail.com>
2023-10-03 21:11:18 -0400

    Migrate to Bun

--- src/lib/server/routers/_app.ts
+++ src/lib/server/routers/_app.ts
@@ -1,8 +1,7 @@
-import { computersRouter } from "./computers";
 import { router } from "../trpc";
 
 export const appRouter = router({
-  computers: computersRouter,
+  
 });
 
 export type AppRouter = typeof appRouter;
--- src/lib/trpc/api.ts
+++ src/lib/trpc/api.ts
@@ -1,10 +1,10 @@
 import { cookies } from "next/headers";
-import { getUserAuth } from "../auth/utils";
 import { appRouter } from "../server/routers/_app";
 import { loggerLink } from "@trpc/client";
 import { experimental_createTRPCNextAppDirServer as createTRPCNextAppDirServer } from "@trpc/next/app-dir/server";
 import { experimental_nextCacheLink as nextCacheLink } from "@trpc/next/app-dir/links/nextCache";
 import SuperJSON from "superjson";
+import {client as sanityClient} from '@/lib/sanity/client'
 
 /**
  * This client invokes procedures directly on the server without fetching over HTTP.
@@ -21,9 +21,8 @@ export const api = createTRPCNextAppDirServer<typeof appRouter>({
           revalidate: 1,
           router: appRouter,
           async createContext() {
-            const { session } = await getUserAuth();
             return {
-              session,
+              sanityClient,
               headers: {
                 cookie: cookies().toString(),
                 "x-trpc-source": "rsc-invoke",
