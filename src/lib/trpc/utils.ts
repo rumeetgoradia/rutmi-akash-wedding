9986da8284ebf7ed74413d18ffee9f984193f933
Rumeet Goradia <rumeet.goradia@gmail.com>
2023-10-03 21:30:06 -0400

    Log VERCEL_URL

--- src/lib/trpc/utils.ts
+++ src/lib/trpc/utils.ts
@@ -1,7 +1,10 @@
 import { env } from "@/lib/env.mjs";
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`;
+  if (env.VERCEL_URL) {
+    console.log("VERCEL_URL", env.VERCEL_URL)
+    return `https://${env.VERCEL_URL}`; 
+  }
   return "http://localhost:3000";
 }
 
