ee9e5350307c05d4b813cbf35f782c2567359f50
Rumeet Goradia <rumeet.goradia@gmail.com>
2023-11-04 17:15:49 -0400

    Make all hero images same height

--- src/components/layout/Content.tsx
+++ src/components/layout/Content.tsx
@@ -2,36 +2,35 @@ import { cn } from "@/lib/utils";
 import Image, { StaticImageData } from "next/image";
 
 const Content: React.FC<{
-	hero: StaticImageData;
-	title?: string;
-	children: React.ReactNode;
+  hero: StaticImageData;
+  title?: string;
+  children: React.ReactNode;
 }> = ({ hero, title, children }) => {
-	return (
-		<div className="w-full">
-			<div className="w-full relative group overflow-hidden">
-				<Image
-					src={hero}
-					alt="Rutmi & Akash"
-					placeholder="blur"
-					className={cn(
-						"w-full",
-						title && "md:group-hover:blur-sm transition-[filter]"
-					)}
-					priority
-				/>
-				{title && (
-					<div className="top-0 left-0 absolute w-full h-full md:opacity-80 group-hover:opacity-100 bg-foreground/70 flex justify-center items-center p-6 text-background transition-opacity">
-						<h2 className="font-meddon text-4xl lg:text-6xl !leading-[1.3]">
-							{title}
-						</h2>
-					</div>
-				)}
-			</div>
-			<div className="mx-auto flex w-full max-w-screen-md flex-col items-center gap-8 bg-background py-12 md:pt-16 max-md:px-8">
-				{children}
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full">
+      <div className="group relative aspect-video w-full overflow-hidden">
+        <Image
+          src={hero}
+          alt="Rutmi & Akash"
+          placeholder="blur"
+          className={cn(title && "transition-[filter] md:group-hover:blur-sm")}
+          priority
+          fill
+          style={{ objectFit: "cover" }}
+        />
+        {title && (
+          <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-foreground/70 p-6 text-background transition-opacity group-hover:opacity-100 md:opacity-80">
+            <h2 className="font-meddon text-4xl !leading-[1.3] lg:text-6xl">
+              {title}
+            </h2>
+          </div>
+        )}
+      </div>
+      <div className="mx-auto flex w-full max-w-screen-md flex-col items-center gap-8 bg-background py-12 max-md:px-8 md:pt-16">
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export { Content };
