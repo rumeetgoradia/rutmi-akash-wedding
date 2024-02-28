1512927d1d5070ddf0715912bebe0946f6c2eedc
Rumeet Goradia <rumeet.goradia@gmail.com>
2024-02-27 20:41:23 -0500

    Change ring color

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "font-figtree mt-0 flex h-10 w-full rounded-sm border border-slate-200 bg-background px-3 py-2 text-[16px] ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "font-figtree mt-0 flex h-10 w-full rounded-sm border border-slate-200 bg-background px-3 py-2 text-[16px] ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
